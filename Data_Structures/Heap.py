d007d63bc961f71fe7e008e77e2a2b8ae6124a31
Surya Shanmugaselvam <amy6zx@virginia.edu>
2023-08-14 19:54:03 -0400

    Finalized heap implementation for priority queue

--- Data_Structures/Heap.py
+++ Data_Structures/Heap.py
@@ -15,6 +15,9 @@ class Node:
         self.metadata = metadata
         self.pos = pos
 
+    def setPos(self, pos):
+        self.pos = pos
+
     def compare(self, comparingNode, compareFunc):
         if isinstance(self.content, int):
             return compareFunc(self.content, comparingNode.content)
@@ -24,8 +27,10 @@ class Heap:
     def __init__(self, compareFunc, root: Node = None):
         self.compareFunc = compareFunc
         self.root = root
-        self.root.pos = 1 if self.root is not None else None
-        self.heap = (list((object, self.root)) if self.root is not None else list(object))
+        if self.root is not None:
+            self.root.setPos(1)
+        firstElement = object()
+        self.heap = (list((firstElement, self.root)) if self.root is not None else [firstElement])
         self.size = (1 if self.root is not None else 0)
 
     def parent(self, node: Node):
@@ -33,9 +38,13 @@ class Heap:
         return self.heap[int(node.pos / 2)]
 
     def leftNode(self, node: Node):
+        if int(node.pos * 2) > self.size:
+            return None
         return self.heap[int(node.pos * 2)]
 
     def rightNode(self, node: Node):
+        if int((node.pos * 2) + 1) > self.size:
+            return None
         return self.heap[int((node.pos * 2) + 1)]
 
     def isLeaf(self, node: Node):
@@ -61,8 +70,8 @@ class Heap:
             else:
                 swapPos = self.leftNode(node).pos
 
-            if self.leftNode(node).compare(node, self.compareFunc) < 0 or self.rightNode(node).compare(node,
-                                                                                                       self.compareFunc) < 0:
+            if self.leftNode(node).compare(node, self.compareFunc) < 0 \
+                    or (self.rightNode(node) is not None and self.rightNode(node).compare(node, self.compareFunc) < 0):
                 nodeSwappedOut = self.swap(node, swapPos)
                 self.heapify(node)
 
@@ -73,14 +82,48 @@ class Heap:
         else:
             node = Node(innerContent=obj, metadata=metadata)
 
-        self.heap[self.size + 1] = node
+        self.heap.append(node)
         self.size += 1
         node.pos = self.size
+        if self.root is None:
+            self.root = node
+            return
         while node.compare(self.parent(node), self.compareFunc) < 0:
             parentNodeSwappedOut = self.swap(node, self.parent(node).pos)
+            if(node.pos == 1):
+                break
 
     def remove(self, node):
         removedPos = node.pos
         self.heap[removedPos] = self.heap[self.size]
+        self.heap[removedPos].setPos(removedPos)
+        del self.heap[-1]
         self.size -= 1
         self.heapify(self.heap[removedPos])
+
+    def pop(self):
+        nextNodeOffTree = self.heap[1]
+        self.remove(nextNodeOffTree)
+        return nextNodeOffTree
+
+    def print(self):
+        return [o.content for o in self.heap[1:]]
+
+
+minHeap = Heap(maxHeapComparator)
+minHeap.insert(3, "root")
+minHeap.insert(4)
+minHeap.insert(32)
+minHeap.insert(102)
+minHeap.insert(16)
+minHeap.insert(2)
+minHeap.insert(49)
+minHeap.insert(65)
+minHeap.insert(83)
+print(minHeap.print())
+
+pq = minHeap
+firstNumberOff = pq.pop().content
+print(f"Next node off {firstNumberOff} --> New Queue is {pq.print()}")
+
+
